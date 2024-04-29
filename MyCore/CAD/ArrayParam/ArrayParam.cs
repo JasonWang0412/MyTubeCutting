fcc6c83f12662287d0ea92360a51f28a2a5147dd
JasonWang0412 <jason970411@gmail.com>
2024-04-29 21:33:33 +0800

    新增 ArrayParam

---
+++ MyCore/CAD/ArrayParam/ArrayParam.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MyCore.CAD
+{
+	[Serializable]
+	public class ArrayParam
+	{
+		public ArrayParam()
+		{
+			LinearCount = 1;
+			LinerDistance = 10;
+			AngularCount = 1;
+			AngularDistance = 30;
+		}
+
+		public int LinearCount
+		{
+			get;
+			set;
+		}
+
+		public double LinerDistance
+		{
+			get;
+			set;
+		}
+
+		public int AngularCount
+		{
+			get;
+			set;
+		}
+
+		public double AngularDistance
+		{
+			get;
+			set;
+		}
+
+		public bool IsValid()
+		{
+			if( LinearCount < 1 || LinerDistance <= 0 || AngularCount < 1 || AngularDistance <= 0 ) {
+				return false;
+			}
+			return true;
+		}
+	}
+}
