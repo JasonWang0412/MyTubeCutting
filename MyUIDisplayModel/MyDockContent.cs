7138d45f9a7aa1e0135728fb9357534951f4a1ec
王棕賢 <jason.wang@syntecclub.com>
2024-06-17 16:45:14 +0800

    補 "修正 dockpanel 可以被 close 掉的問題 (b25dd8)" 遺漏的檔案

---
+++ MyUIDisplayModel/MyDockContent.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace MyUIDisplayModel
+{
+	public class MyDockContent : DockContent
+	{
+		public MyDockContent()
+		{
+			FormClosing += MyDockContent_FormClosing;
+		}
+
+		void MyDockContent_FormClosing( object sender, FormClosingEventArgs e )
+		{
+			e.Cancel = true;
+		}
+	}
+}
